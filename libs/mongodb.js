ebca13e74fc6544ff02cbb785fecd59689b486d7
Deepika157 <negi.deepika.2017742@gmail.com>
2024-02-19 17:54:52 +0530

    Crud app

---
+++ libs/mongodb.js
@@ -0,0 +1,15 @@
+
+import mongoose from "mongoose";
+
+const connectMongoDB=async  () => {
+    try{
+        //console.log("asdfklajsdlfkjsd<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<", process.env.MONGODB_URI)
+        await  mongoose.connect(process.env.MONGODB_URI);
+         console.log("Connected to MongoDB");
+    }
+    catch(error){
+        console.log(error);
+    }
+};
+
+export default connectMongoDB;
